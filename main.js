e2cbec85491faf7a1401302c98142690f1bdbf56
kimkichan1225 <kimkichan1225@users.noreply.github.com>
2026-04-15 12:38:15 +0900

    토끼 캐릭터 추가 (1:1 비율 유지 축소 + row 2(RIGHT) → row 3, row 2는 플립)

--- main.js
+++ main.js
@@ -736,6 +736,7 @@ ipcMain.handle('read-svg', async (event, gender) => {
   else if (g === 'sprigatito') fileName = 'pixelated-cartoon-sprigatito.png';
   else if (g === 'slime') fileName = 'pixelated-cartoon-slime.svg';
   else if (g === 'cat2') fileName = 'pixelated-cartoon-cat2.png';
+  else if (g === 'rabbit') fileName = 'pixelated-cartoon-rabbit.png';
   else fileName = 'pixelated-cartoon-boy.svg';
   // PNG 파일은 base64 data URL로 반환
   if (fileName.endsWith('.png')) {
