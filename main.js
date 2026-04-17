73ef17cfe437a4f1c83dd4a620311efe315645f3
kimkichan1225 <kimkichan1225@users.noreply.github.com>
2026-04-17 13:30:42 +0900

    고양이3 캐릭터 추가 (row 3 col 0을 row 2 col 1/3에 복사 + row 3 = row 2 좌우반전)

--- main.js
+++ main.js
@@ -773,6 +773,7 @@ ipcMain.handle('read-svg', async (event, gender) => {
   else if (g === 'nezuco') fileName = 'pixelated-cartoon-nezuco.png';
   else if (g === 'calcifer') fileName = 'pixelated-cartoon-calcifer.png';
   else if (g === 'pantom') fileName = 'pixelated-cartoon-pantom.png';
+  else if (g === 'cat3') fileName = 'pixelated-cartoon-cat3.png';
   else fileName = 'pixelated-cartoon-boy.svg';
   // PNG 파일은 base64 data URL로 반환
   if (fileName.endsWith('.png')) {
